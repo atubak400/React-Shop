8e7db698f2ea3776ffe6ace36be98581f462db8f
Kingsley Atuba <atubak40@gmail.com>
2021-03-09 14:29:31 +0100

    grid view

--- src/components/GridView.js
+++ src/components/GridView.js
@@ -5,7 +5,7 @@ import Product from './Product'
 const GridView = ({ products }) => {
 	return (
 		<Wrapper>
-			<div className='products-container'>
+			<div className='product-container'>
 				{products.map((product) => {
 					return <Product key={product.id} {...product} />
 				})}

eb65be3fd1271695639e22d0317f2ad3af38f14e
Kingsley Atuba <atubak40@gmail.com>
2021-03-10 12:53:11 +0100

    filter products

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -15,7 +15,18 @@ import { useProductsContext } from './products_context'
 const initialState = {
 	filtered_products: [],
 	all_products: [],
-	grid_view: false,
+	grid_view: true,
+	sort: 'price-lowest',
+	filters: {
+		text: '',
+		company: 'all',
+		category: 'all',
+		color: 'all',
+		min_price: 0,
+		max_price: 0,
+		price: 0,
+		shipping: false,
+	},
 }
 
 const FilterContext = React.createContext()
@@ -28,14 +39,28 @@ export const FilterProvider = ({ children }) => {
 		dispatch({ type: LOAD_PRODUCTS, payload: products })
 	}, [products])
 
+	useEffect(() => {
+		dispatch({ type: SORT_PRODUCTS })
+	}, [products, state.sort, state.filters])
+
 	const setGridView = () => {
 		dispatch({ type: SET_GRIDVIEW })
 	}
 	const setListView = () => {
 		dispatch({ type: SET_LISTVIEW })
 	}
+	const updateSort = (e) => {
+		// for demonstration
+		// const name = e.target.name
+		const value = e.target.value
+		dispatch({ type: UPDATE_SORT, payload: value })
+	}
 
-	return <FilterContext.Provider value={{ ...state, setGridView, setListView }}>{children}</FilterContext.Provider>
+	return (
+		<FilterContext.Provider value={{ ...state, setGridView, setListView, updateSort }}>
+			{children}
+		</FilterContext.Provider>
+	)
 }
 // make sure use
 export const useFilterContext = () => {

4223413cf5919f45c4565031122644e2f6d67747
Kingsley Atuba <atubak40@gmail.com>
2021-03-12 09:17:42 +0100

    cart reducer

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,7 +6,7 @@ import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
 
 const AddToCart = ({ product }) => {
-	// const { addToCart } = useCartContext()
+	const { addToCart } = useCartContext()
 	const { id, stock, colors } = product
 	console.log(stock)
 
@@ -53,7 +53,7 @@ const AddToCart = ({ product }) => {
 			</div>
 			<div className='btn-container'>
 				<AmountButtons amount={amount} increase={increase} decrease={decrease} />
-				<Link to='/cart' className='btn'>
+				<Link to='/cart' className='btn' onClick={() => addToCart(id, mainColor, amount, product)}>
 					add to cart
 				</Link>
 			</div>
@@ -107,5 +107,3 @@ const Wrapper = styled.section`
 	}
 `
 export default AddToCart
-
-// onClick={() => addToCart(id, mainColor, amount, product)}
